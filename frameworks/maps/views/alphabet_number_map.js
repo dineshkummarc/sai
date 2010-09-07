ccca87e100a6cb7269c39e92e0ab84df2e4803fc
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-09-07 13:15:10 -0500

    adding first files to maps framework

---
+++ frameworks/maps/views/alphabet_number_map.js
@@ -0,0 +1,140 @@
+// ..........................................................
+// A basic Line chart
+// 
+/*globals Sai */
+sc_require('views/basemap');
+
+Sai.AlphabetNumberMapView = Sai.BaseMapView.extend({
+  
+  // ..........................................................
+  // Properties
+  
+  // @param data: This is an array of arrays of pairs for the cells
+  // @example: [[1,2], [5,6], [8,3]]
+  // Cell #1: "1, 2"
+  // Cell #2: "5, 6"
+  // Cell #3: "8, 3"
+  //
+  // where each coordinate represents a number/letter_string pair. Numbers
+  // are integer numbers for the x-axis. Letter strings on such maps are usually
+  // placed on the y-axix. Letter strings are represented by
+  // an integer index into the 26-letter alphabet, or multiples thereof.
+  // So, we have: 
+  //
+  //   A  ...   Z    AA  ...  ZA   AB ...  ZB
+  //   1  ...  26    27  ...  52   53 ...  78 etc.
+  //
+  // If my memory serves, a map cell is referred to as a letter_string/number
+  // pair (y/x), which is switched from normal x/y order. For example,
+  // cells would be referred to as A13, J24, CB42.
+  //
+  alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+
+  // We only handle one- or two-letter letter_strings. (26 combinations of 26)
+  twoLetterLimit: 26 * 26,
+
+  data: null,
+  
+  // @param: dataAttrs - Hash of styling parameters
+  // @example: {fill: "#000", stroke: "#00", opacity: 0.4}
+  dataAttrs: null,
+  
+  // @param grid: show a grid for all the points
+  grid: null,
+  
+  leftAxis: null,
+  rightAxis: null,
+  bottomAxis: null,
+  topAxis: null,
+  
+  displayProperties: 'data dataAttrs grid leftAxis rightAxis bottomAxis topAxis'.w(),
+  
+  renderCanvas: function(canvas, firstTime) {
+    var grid = this.get('grid'),
+        d = this.get('data') || [],
+        dAttrs = this.get('dataAttrs') || {colors: 'black'},
+        f = this.get('frame'), axis;
+
+    if(d.length === 0) return;
+
+    if (!firstTime) canvas.clear();  
+
+    axes = this._makeAxes(f, canvas, d) || [];
+
+    _plotCells(f, canvas, d, dAttrs, axes[0], axes[1], axes[2], axes[3]);
+  },
+  
+  _makeAxes: function(f, canvas, d){
+    var axis, path, tCount, space, offset, tmp, aa,
+        yaLeft = this.get('leftAxis') || {}, yScale,
+        yaRight = this.get('rightAxis') || {}, 
+        xaBottom = this.get('bottomAxis') || {},
+        xaTop = this.get('topAxis') || {},
+        xMargin = xaBottom.margin || 0.1,
+        yMargin = yaLeft.margin || 0.1,
+        startX = f.width*yMargin,
+        endX = f.width*0.95,
+        // Y coordinate stuff
+        startY = f.height*(1.0 - xMargin),
+        endY = f.height*0.05, dLen = d.length || 0;
+        
+    // X axes
+    if (xaBottom){
+      // Calculate the coordinate system
+      xaBottom.coordMin = startX;
+      xaBottom.coordMax = endX;
+      if (SC.none(xaBottom.hidden) || !xaBottom.hidden) this.makeAxis(canvas, startX, startY, endX, startY, xaBottom, {direction: 'x-bottom', len: 5, count: tCount, space: xaBottom.space, offset: xaBottom.offset});
+    }
+    if (xaTop){
+      // Calculate the coordinate system
+      xaTop.coordMin = startX;
+      xaTop.coordMax = endX;
+      if (SC.none(xaTop.hidden) || !xaTop.hidden) this.makeAxis(canvas, startX, startY, endX, startY, xaTop, {direction: 'x-top', len: 5, count: tCount, space: xaTop.space, offset: xaTop.offset});
+    }
+
+    // Y axes
+    if (yaLeft){
+      yaLeft.coordMin = startY;
+      yaLeft.coordMax = endY;
+      if (SC.none(yaLeft.hidden) || !yaLeft.hidden) this.makeAxis(canvas, startX, startY, startX, endY, yaLeft, {direction: 'y-left', len: 5, count: tCount, space: yaLeft.space, offset: yaLeft.offset});
+    }
+    if (yaRight){
+      yaRight.coordMin = startY;
+      yaRight.coordMax = endY;
+      if (SC.none(yaRight.hidden) || !yaRight.hidden) this.makeAxis(canvas, startX, startY, startX, endY, yaRight, {direction: 'y-right', len: 5, count: tCount, space: yaRight.space, offset: yaRight.offset});
+    }
+    
+    return [xaBottom, xaTop, yaLeft, yaRight];
+  }, 
+
+  _plotCells: function(f, canvas, d, dAttrs, leftAxis, rightAxis, bottomAxis, topAxis){
+    var x, xSpace = bottomAxis.space,
+        xOffset = (xSpace*bottomAxis.offset), 
+        y, ySpace = leftAxis.space,
+        yOffset = (ySpace*leftAxis.offset), 
+        colors = dAttrs.color || dAttrs.colors || 'blue';
+    d.forEach( function(point) {
+      x = bottomAxis.coordMin + (point[1] * xSpace) - (0.5 * xSpace);
+      y = leftAxis.coordMin + (point[0] * ySpace) - (0.5 * ySpace);
+      canvas.rectangle(x, y, xSpace, ySpace, 0, {stroke: colors[j], fill: colors[j]}, 'cell-%@-%@'.fmt(this._getLetterString(x),y));
+    });
+  },
+
+  _getLetterString: function(index) {
+    // We only handle one- or two-letter letter_strings.
+    //
+    if (index < 26) {
+      return this.alphabet.charAt(index);
+    }
+    else if (index < this.twoLetterLimit) {
+      var multiple = index / 26;
+      var secondLetterIndex = alphabet.indexOf(multiple);
+      var secondLetter = this._getLetter(secondLetterIndex);
+      return this.alphabet.charAt(index % 26) + secondLetter;
+    }
+    else {
+      return 'ZZZ';
+    }
+  }
+});
+
---
+++ frameworks/maps/views/basemap.js
@@ -0,0 +1,135 @@
+// ..........................................................
+// A foundation chart that has axis
+// 
+/*globals Sai */
+
+Sai.BaseMapView = Sai.CanvasView.extend({
+
+  makeAxis: function(canvas, sx, sy, ex, ey, axisAttrs, ticks){
+    var path, i, len, dir, tLen, tickPts = {}, currTick, tickLabels = [],
+        space, tp, tOff, tickFunc, rounder = this.rounder, step;
+    
+    axisAttrs = axisAttrs || {};
+    step = axisAttrs.step || 1;
+    // Draw the line to the end
+    path = 'M%@1,%@2L%@3,%@4M%@1,%@2'.fmt(rounder(sx), rounder(sy), rounder(ex), rounder(ey));
+    if (ticks){
+      dir = ticks.direction;
+      tLen = ticks.len;
+      space = ticks.space;
+      
+      // Find the right tick intremental function based off of the axis (X or Y)
+      tickFunc = (dir === 'x-bottom' || dir === 'x-top') ? function(x,y,sp){ return [x, (y+tLen), (x+sp), y]; } : function(x,y,sp){ return [(x-tLen), y, x, (y-sp)]; };
+      
+      // Some times you want to ofset the start of the ticks to center
+      tOff = ticks.offset || 0;
+      if (tOff > 0 && tOff < 1){
+        tp = tickFunc(sx,sy,space*tOff);
+        sx = tp[2];
+        sy = tp[3];
+        path += 'M%@,%@'.fmt(rounder(sx), rounder(sy));
+      }
+      
+      // Draw all the ticks
+      for(i = 0, len = ticks.count; i < len; i++){
+        tp = tickFunc(sx,sy,space);
+        sx = tp[2];
+        sy = tp[3];
+        currTick = {x: rounder(tp[0]), y: rounder(tp[1])};
+        path += 'L%@,%@M%@,%@'.fmt(currTick.x, currTick.y, rounder(tp[2]), rounder(tp[3]));
+        tickPts[i] = {t: currTick, idx: i*step};
+        tickLabels.push(''+i*step);
+      }
+    }
+    //console.log('Axis Path: '+path);
+    
+    // Do Labels
+    if (!SC.none(axisAttrs.labels)) this.makeLabels(canvas, tickPts, axisAttrs, ticks, tickLabels);
+    
+    canvas.path(path, {stroke: axisAttrs.color || 'black', strokeWidth: axisAttrs.weight || 1}, '%@-axis'.fmt(dir));
+  },
+  
+  makeLabels: function(canvas, tickPts, axisAttrs, ticks, tLabels){
+    var dir, labels, l, lAttrs, tick, aa, t, labelPosFunc, col,
+        lWidth, lHeight, lOff;
+    
+    aa = axisAttrs || {};
+    dir = ticks ? ticks.direction || 'x-bottom' : 'x-bottom';
+    labels = aa.labels || [];
+    lAttrs = aa.labelAttrs || {};
+    lWidth = lAttrs.width || ticks.space*0.9 || 50;
+    lHeight = lAttrs.height || 15;
+    // FIXME: [EG] HATE THIS...need to find out how to calulate the middle point of a text
+    lOff = lAttrs.offset || 0;
+    col = aa.labelColor || aa.color || 'black';
+    
+    // Create the label positioning function
+    if (dir === 'x-bottom'){
+      labelPosFunc = function(t, label){ 
+        var x, y;
+        x = +t.x - (lWidth/2);
+        y = +t.y + lOff;
+        canvas.text(x, y, lWidth, lHeight, label, {fill: col, stroke: col, textAnchor: 'center', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+        // canvas.rectangle(x, y, lWidth, lHeight, 0, {fill: aa.labelColor || aa.color || 'black', textAnchor: 'center', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+      };
+    }
+    else if(dir === 'x-top') {
+      labelPosFunc = function(t, label){ 
+        var x, y;
+        x = +t.x - (lWidth/2);
+        y = +t.y - (lHeight/2) - lOff;
+        canvas.text(x, y, lWidth, lHeight, label, {fill: col, stroke: col, textAnchor: 'center', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+        // canvas.rectangle(x, y, lWidth, lHeight, 0, {fill: aa.labelColor || aa.color || 'black', textAnchor: 'center', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+      };
+    }
+    else if(dir === 'y-left') {
+      labelPosFunc = function(t, label){ 
+        var x, y;
+        x = t.x - lWidth;
+        y = t.y - (lHeight/2) + lOff;
+        canvas.text(x, y, lWidth, lHeight, label, {fill: col, stroke: col, textAnchor: 'right', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+        // canvas.rectangle(x, y, lWidth, lHeight, 0, {fill: aa.labelColor || aa.color || 'black', textAnchor: 'right', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+      };
+    }
+    else if(dir === 'y-right') {
+      labelPosFunc = function(t, label){ 
+        var x, y;
+        x = t.x + lOff;
+        y = t.y - (lHeight/2) + lOff;
+        canvas.text(x, y, lWidth, lHeight, label, {fill: col, stroke: col, textAnchor: 'right', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+        // canvas.rectangle(x, y, lWidth, lHeight, 0, {fill: aa.labelColor || aa.color || 'black', textAnchor: 'right', fontSize: lAttrs.fontSize}, 'label-%@'.fmt(label));
+      };
+    }
+    
+    if (SC.typeOf(labels) === SC.T_HASH){ 
+      this._generateIncrementalLabels(tickPts, labels, labelPosFunc, YES);
+    }
+    else if (SC.typeOf(labels) === SC.T_ARRAY){
+      this._generateIncrementalLabels(tickPts, labels, labelPosFunc, NO);
+    }
+    else if (SC.typeOf(labels) === SC.T_BOOL){
+      this._generateIncrementalLabels(tickPts, tLabels, labelPosFunc, NO);
+    }
+  },
+  
+  _generateIncrementalLabels: function(pts, labels, func, useIndex){
+    var tick, t, l, idx;
+    for (t in pts){
+      tick = pts[t].t;
+      idx = pts[t].idx;
+      l = useIndex ? labels[idx] : labels[t];
+      if (!SC.none(tick) && l) func(tick, l);
+    }
+  },
+  
+  makeGrid: function(){
+    // TODO: [EG] make the grid
+  },
+  
+  rounder: function(x){
+    if (x > (~~x+0.00051)) return x.toFixed(3);
+    return x.toFixed(0);
+  }
+  
+});
+
