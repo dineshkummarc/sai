b44b10255aec8a5ad120c3cc9224dddd61c66908
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-09-15 12:51:08 -0500

    changed hex code in resetColor test from #FFB946 to #BF5600

--- frameworks/foundation/tests/helpers/HSB.java
+++ frameworks/foundation/tests/helpers/HSB.java
@@ -91,6 +91,8 @@ class HSB {
     printHSB(0.500f, 1.0f,  1.0f);
     System.out.println("0.0f, 1.0f,  0.75f");
     printHSB(0.0f, 1.0f,  0.75f);
+    System.out.println("0.075f, 1.0f,  0.75f");
+    printHSB(0.075f, 1.0f,  0.75f);
   }
 }
 
